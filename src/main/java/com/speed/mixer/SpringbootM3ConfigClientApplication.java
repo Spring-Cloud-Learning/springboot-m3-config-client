69cbbcc33d47439c5ae82f885ae375a366ac4f1c
unknown <sambitmishra987@gmail.com>
2018-01-23 11:36:01 +0530

    springboot-m3-config-client initial commit

---
+++ src/main/java/com/speed/mixer/SpringbootM3ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.speed.mixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootM3ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootM3ConfigClientApplication.class, args);
+	}
+}
